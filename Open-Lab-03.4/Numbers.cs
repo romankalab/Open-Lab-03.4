30c3269990574e201cc71ed247ece9bcd0835bb6
romankalab <roan.kalab@gmail.com>
2019-10-24 09:19:35 +0200

    Initial

---
+++ Open-Lab-03.4/Numbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EvenOrOdd
+{
+    class Numbers
+    {
+        public static string EvenOrOdd(int number)
+        {
+            string a = "even";
+            string b = "odd";
+            if (number % 2 == 0)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+    }
+}
---
+++ Open-Lab-03.4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using EvenOrOdd;
+
+namespace Open_Lab_03._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
